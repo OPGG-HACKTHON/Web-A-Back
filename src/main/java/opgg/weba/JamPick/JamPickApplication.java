df38b54fe78f53e7a0f08953e07f101bcba9dd5b
jihwan <sgh002400@naver.com>
2021-07-31 04:07:43 +0900

    Project Setting

---
+++ src/main/java/opgg/weba/JamPick/JamPickApplication.java
@@ -0,0 +1,13 @@
+package opgg.weba.JamPick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamPickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamPickApplication.class, args);
+	}
+
+}
---
+++ src/test/java/opgg/weba/JamPick/JamPickApplicationTests.java
@@ -0,0 +1,13 @@
+package opgg.weba.JamPick;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JamPickApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
