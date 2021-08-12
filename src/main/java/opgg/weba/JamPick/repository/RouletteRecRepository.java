cc23b1534f7647385450746543fc593930f8337b
Jihwan <sgh002400@naver.com>
2021-08-12 15:37:03 +0900

    [FIX] #11 - Fix RouletteRecommandtion logic, RouletteRecRepositoryTest logic

--- src/main/java/opgg/weba/JamPick/repository/RouletteRecRepository.java
+++ src/main/java/opgg/weba/JamPick/repository/RouletteRecRepository.java
@@ -1,13 +1,12 @@
 package opgg.weba.JamPick.repository;
 
 import lombok.RequiredArgsConstructor;
-import opgg.weba.JamPick.domain.IndieApp;
-import opgg.weba.JamPick.dto.RouletteRecDTO;
+import opgg.weba.JamPick.dto.RouletteRecCriteriaDto;
+import opgg.weba.JamPick.dto.RouletteRecDto;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
-import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
@@ -15,9 +14,16 @@ public class RouletteRecRepository {
 
     private final EntityManager em;
 
-    public RouletteRecDTO findOne() {
-        TypedQuery<RouletteRecDTO> query = em.createQuery("select new opgg.weba.JamPick.dto.RouletteRecDTO(i.id, i.header_image) from IndieApp i", RouletteRecDTO.class);
-        RouletteRecDTO result = query.getSingleResult();
+    public RouletteRecDto findOne(RouletteRecCriteriaDto request) {
+
+        //TODO -> where g.desc=:request에서 문제가 발생한다!
+
+        TypedQuery<RouletteRecDto> query = em.createQuery( //TODO -> language 컬럼에 저장된 값 중 하나로 고정하기
+                "select new opgg.weba.JamPick.dto.RouletteRecDto(i.id, i.headerImage)" +
+                        " from IndieApp i join i.genres g where g.description='호러'", RouletteRecDto.class);
+
+
+        RouletteRecDto result = query.getSingleResult(); //TODO -> json 내에서 객체 안에 담겨 있게 하기 위함인데 바로 리턴해도 객체로 감싸지는지 확인하기
 
         return result;
     }
---
+++ src/test/java/opgg/weba/JamPick/controller/RouletteRecControllerTest.java
@@ -0,0 +1,16 @@
+package opgg.weba.JamPick.controller;
+
+import opgg.weba.JamPick.domain.IndieApp;
+import opgg.weba.JamPick.repository.RouletteRecRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootTest
+public class RouletteRecControllerTest {
+
+
+}
