d694023ab7f0e2b387c4108e72e55b2977295d6e
CHOI HEE JAE <gmlwo530@gmail.com>
2021-08-16 21:02:21 +0900

    [FEAT] #32 - Create test code for exception package

--- src/test/java/opgg/weba/JamPick/exception/DemoExceptionTest.java
+++ src/test/java/opgg/weba/JamPick/exception/DemoExceptionTest.java
@@ -1,2 +1,73 @@
-package opgg.weba.JamPick.exception;public class DemoExceptionTest {
+package opgg.weba.JamPick.exception;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+public class DemoExceptionTest {
+
+    @Test
+    void testExceptionType() {
+        for (ExceptionType et : ExceptionType.values()) {
+            assertThat(et.value).isEqualTo(et.getValue());
+        }
+    }
+
+    @Test
+    void testEntityType() {
+        Set<String> classNames = findAllClassNames("opgg.weba.JamPick.domain");
+        EntityType[] entityTypes = EntityType.values();
+
+        assertThat(entityTypes.length).isEqualTo(classNames.size());
+
+        for (EntityType et : entityTypes) {
+            assertThat(et.value).isEqualTo(et.getValue());
+            assertThat(classNames.contains(et.getValue())).isTrue();
+        }
+    }
+
+    @Test
+    void testJampickExceptionWithMessage() {
+        String msgTemplate = "Test Exception {0} {1}";
+
+        assertThat(JampickException.throwException(msgTemplate, "arg1", "arg2").getMessage()).isEqualTo(
+                String.format(msgTemplate, "arg1", "arg2")
+        );
+    }
+
+    @Test
+    void testJampickExceptionWithEntity() {
+        for (ExceptionType exceptionType : ExceptionType.values()) {
+            String msgTemplate = null;
+            if (exceptionType.equals(ExceptionType.ENTITY_EXCEPTION)) {
+                msgTemplate = JampickException.ENTITY_EXCEPTION_TEMPLATE;
+            } else if (exceptionType.equals(ExceptionType.ENTITY_NOT_FOUND)) {
+                msgTemplate = JampickException.ENTITY_NOT_FOUND_TEMPLATE;
+            }
+
+            for (EntityType entityType : EntityType.values()) {
+                assertThat(JampickException.throwException(entityType, exceptionType, "arg1").getMessage()).isEqualTo(
+                        String.format(msgTemplate, entityType.getValue(), "arg1")
+                );
+            }
+        }
+    }
+
+    private Set<String> findAllClassNames(String packageName) {
+        InputStream stream = ClassLoader.getSystemClassLoader()
+                .getResourceAsStream(packageName.replaceAll("[.]", "/"));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+        return reader.lines()
+                .filter(line -> line.endsWith(".class"))
+                .map(line -> packageName + "." + line.substring(0, line.lastIndexOf(".")))
+                .collect(Collectors.toSet());
+    }
 }
