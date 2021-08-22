8f7c2ef1675418897869d58ef9b7d23e9f4206aa
CHOI HEE JAE <gmlwo530@gmail.com>
2021-08-23 03:03:43 +0900

    [FEAT] #8 - Edit test util

--- src/test/java/opgg/weba/JamPick/util/Util.java
+++ src/test/java/opgg/weba/JamPick/util/Util.java
@@ -1,17 +1,23 @@
 package opgg.weba.JamPick.util;
 
 import opgg.weba.JamPick.domain.*;
+import opgg.weba.JamPick.repository.IndieAppRepository;
 
+import javax.persistence.EntityManager;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class Util {
-    public static final String KR_LANG = "kr_KR";
-    public static final String EN_LANG = "en_US";
 
-    public static final Long[] GENRE_IDS = {2L, 37L, 70L, 28L, 25L, 18L, 51L, 51L, 9L, 74L, 53L, 80L, 29L, 82L, 52L, 85L, 4L, 1L, 57L, 3L};
+public class Util {
+    public static final String KR_LANG = Locale.KOREA.toString();
+    public static final String EN_LANG = Locale.ENGLISH.toString();
+    public static final Long[] GENRE_IDS = {2L, 37L, 70L, 28L, 25L, 18L, 51L, 100L, 9L, 74L, 53L, 80L, 29L, 82L, 52L, 85L, 4L, 1L, 57L, 3L};
     public static final String[] KR_GENRE_DESCRIPTION = {"전략", "무료", "앞서 해보기", "시뮬레이션", "어드벤처", "스포츠", "애니메이션", "모델링", "레이싱", "고어", "일러스트레이션", "영화", "멀티플레이어", "에피소드", "오디오 제작", "360동영상", "캐주얼", "액션", "유틸리티", "RPG"};
     public static final String[] EN_GENRE_DESCRIPTION = {"Tactics", "Free", "Beta", "Simulation", "Adventure", "Sports", "Animation", "Modeling", "Racing", "Gore", "Illustration", "Movie", "Multiplayer", "Episode", "Audio", "360 Movie", "Casual", "Action", "Utility", "RPG"};
+
+    public static String[] getLanguages() {
+        return new String[]{KR_LANG, EN_LANG};
+    }
 
     public static String getRandomText(int targetStringLength) {
         int leftLimit = 48; // numeral '0'
@@ -29,53 +35,34 @@ public class Util {
         return "https://" + getRandomText(20);
     }
 
-    public static List<Genre> createGenres(IndieApp indieApp, Integer count, String lang) {
+    public static List<Genre> createGenres(EntityManager em, IndieApp indieApp, Integer count) {
         ArrayList<Genre> genres = new ArrayList<>();
 
-        Random random = new Random();
+        for (String lang : new String[]{KR_LANG, EN_LANG}) {
+            Random random = new Random();
 
-        for (int i = 0; i < count; i++) {
-            int val = random.nextInt(GENRE_IDS.length);
+            Set<Integer> set = new LinkedHashSet<>();
+            while (set.size() < count) {
+                set.add(random.nextInt(GENRE_IDS.length));
+            }
 
-            Genre genre = new Genre();
-            genre.setGenreId(GENRE_IDS[val]);
-            genre.setDescription(lang.equals(KR_LANG) ? KR_GENRE_DESCRIPTION[val] : EN_GENRE_DESCRIPTION[val]);
-            genre.setLanguage(lang);
-            genre.setIndieApp(indieApp);
-            genres.add(genre);
+            for (Integer i : set) {
+                Genre genre = new Genre();
+                genre.setGenreId(GENRE_IDS[i]);
+                genre.setDescription(lang.equals(KR_LANG) ? KR_GENRE_DESCRIPTION[i] : EN_GENRE_DESCRIPTION[i]);
+                genre.setLanguage(lang);
+                genre.setIndieApp(indieApp);
+                em.merge(genre);
+                genres.add(genre);
+            }
         }
+
+        System.out.println(genres.size());
 
         return genres;
     }
 
-    public static IndieApp createIndieApp(Long id, Boolean isCreateChildData) {
-        IndieApp indieApp = new IndieApp();
-
-        indieApp.setId(id);
-        indieApp.setCcu(0);
-        indieApp.setAverageForever(0);
-        indieApp.setName(getRandomText(10));
-        indieApp.setHeaderImage(getRandomUrl());
-
-        if (isCreateChildData) {
-            ArrayList<Genre> genres = new ArrayList<>();
-            ArrayList<IndieAppDetail> indieAppDetails = new ArrayList<>();
-            ArrayList<Movie> movies = createMovies(indieApp, 5);
-            ArrayList<Screenshot> screenshots = createScreenshots(indieApp, 4);
-
-            for (String lang : new String[] {KR_LANG, EN_LANG}) {
-                genres.addAll(createGenres(indieApp, 6, lang));
-                indieAppDetails.add(createIndieAppDetail(indieApp, lang));
-            }
-
-            indieApp.setGenres(genres);
-            indieApp.setIndieAppDetails(indieAppDetails);
-        }
-
-        return indieApp;
-    }
-
-    public static ArrayList<Screenshot> createScreenshots(IndieApp indieApp, int count) {
+    public static ArrayList<Screenshot> createScreenshots(EntityManager em, IndieApp indieApp, int count) {
         ArrayList<Screenshot> screenshots = new ArrayList<>();
 
         for (int i = 1; i < count + 1; i++) {
@@ -84,13 +71,14 @@ public class Util {
             screenshot.setPathFull(getRandomUrl());
             screenshot.setPathThumbnail(getRandomUrl());
             screenshot.setIndieApp(indieApp);
+            em.merge(screenshot);
             screenshots.add(screenshot);
         }
 
         return screenshots;
     }
 
-    public static ArrayList<Movie> createMovies(IndieApp indieApp, int count) {
+    public static ArrayList<Movie> createMovies(EntityManager em, IndieApp indieApp, int count) {
         ArrayList<Movie> movies = new ArrayList<>();
 
         for (int i = 1; i < count + 1; i++) {
@@ -99,21 +87,28 @@ public class Util {
             movie.setName(getRandomText(4));
             movie.setMp4(getRandomUrl());
             movie.setIndieApp(indieApp);
+            em.merge(movie);
             movies.add(movie);
         }
 
         return movies;
     }
 
-    public static IndieAppDetail createIndieAppDetail(IndieApp indieApp, String lang) {
-        IndieAppDetail indieAppDetail = new IndieAppDetail();
-//        indieAppDetail.setId(indieApp.getId());
-        indieAppDetail.setLanguage(lang);
-        indieAppDetail.setShortDescription(getRandomText(50));
-        indieAppDetail.setReleaseDate(getRandomDateString(lang));
-        indieAppDetail.setIndieApp(indieApp);
+    public static List<IndieAppDetail> createIndieAppDetail(EntityManager em, IndieApp indieApp) {
+        ArrayList<IndieAppDetail> indieAppDetails = new ArrayList<>();
 
-        return indieAppDetail;
+        for (String lang : new String[]{KR_LANG, EN_LANG}) {
+            IndieAppDetail indieAppDetail = new IndieAppDetail();
+            indieAppDetail.setId(lang.equals(KR_LANG) ? indieApp.getId() : indieApp.getId() + 1);
+            indieAppDetail.setLanguage(lang);
+            indieAppDetail.setShortDescription(getRandomText(50));
+            indieAppDetail.setReleaseDate(getRandomDateString(lang));
+            indieAppDetail.setIndieApp(indieApp);
+            em.merge(indieAppDetail);
+            indieAppDetails.add(indieAppDetail);
+        }
+
+        return indieAppDetails;
     }
 
     public static String getRandomDateString(String lang) {
@@ -125,5 +120,18 @@ public class Util {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern, locale);
 
         return simpleDateFormat.format(date);
+    }
+
+    public static IndieApp createIndieApp(IndieAppRepository indieAppRepository, Long id) {
+        IndieApp indieApp = new IndieApp();
+
+        indieApp.setId(id);
+        indieApp.setCcu(0);
+        indieApp.setAverageForever(0);
+        indieApp.setName(getRandomText(10));
+        indieApp.setHeaderImage(getRandomUrl());
+
+//        return indieAppRepository.save(indieApp);
+        return indieApp;
     }
 }
