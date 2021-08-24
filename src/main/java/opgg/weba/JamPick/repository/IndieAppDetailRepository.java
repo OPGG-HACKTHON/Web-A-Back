9da4aa3c0f40e221274683a323e1e6cd7ea71574
Jihwan <sgh002400@naver.com>
2021-08-24 20:05:50 +0900

    [FIX] - Fix UnsatisfiedDependencyException

--- src/main/java/opgg/weba/JamPick/repository/IndieAppDetailRepository.java
+++ src/main/java/opgg/weba/JamPick/repository/IndieAppDetailRepository.java
@@ -7,5 +7,4 @@ import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 
 public interface IndieAppDetailRepository extends JpaRepository<IndieAppDetail, Long> {
-    Optional<IndieAppDetail> findByIndieApp_IdAndLanguage(@Param(value = "indieAppId") Long indieAppId, String language);
-}
+    Optional<IndieAppDetail> findByIndieApp_IdAndLanguage(@Param(value = "indieAppId") Long indieAppId, @Param(value="language") String language);}
