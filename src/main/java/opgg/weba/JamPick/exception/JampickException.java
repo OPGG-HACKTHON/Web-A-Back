2a5fb6aae07b94ab9fc1b1df44571b73c923e25d
CHOI HEE JAE <gmlwo530@gmail.com>
2021-09-04 05:58:17 +0900

    [REFACTOR] #50 Edit exception package

--- src/main/java/opgg/weba/JamPick/exception/JampickException.java
+++ src/main/java/opgg/weba/JamPick/exception/JampickException.java
@@ -2,13 +2,14 @@ package opgg.weba.JamPick.exception;
 
 import org.springframework.stereotype.Component;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
 
 @Component
 public class JampickException {
-    public final static String ENTITY_NOT_FOUND_TEMPLATE = "Requested {0} does not exists. Requested value: {1}";
-    public final static String ENTITY_EXCEPTION_TEMPLATE = "Requested {0} raise exception. Detail: {1}";
+    public final static String ENTITY_NOT_FOUND_TEMPLATE = "요청한 {0}은 존재하지 않습니다. 요청 값: {1}";
+    public final static String ENTITY_EXCEPTION_TEMPLATE = "오류가 발생 했습니다. Detail: {0}";
 
     public static RuntimeException throwException(String template, String... args) {
         return new RuntimeException(format(template, args));
@@ -19,7 +20,7 @@ public class JampickException {
     }
 
     private static String format(String template, String... args) {
-        return String.format(template, (Object[]) args);
+        return MessageFormat.format(template, (Object[]) args);
     }
 
     private static String format(EntityType entityType, ExceptionType exceptionType, String... args) {
@@ -27,11 +28,11 @@ public class JampickException {
         argList.add(0, entityType.getValue());
 
         if (ExceptionType.ENTITY_NOT_FOUND.equals(exceptionType)) {
-            return String.format("Requested {0} does not exists. Requested value: {1}", argList.toArray());
+            return MessageFormat.format(ENTITY_NOT_FOUND_TEMPLATE, argList.toArray());
         } else if (ExceptionType.ENTITY_EXCEPTION.equals(exceptionType)) {
-            return String.format("Requested {0} raise exception. Detail: {1}", argList.toArray());
+            return MessageFormat.format(ENTITY_EXCEPTION_TEMPLATE, argList.toArray());
         }
 
-        throw throwException("Unsupported exception type. Passed exception type: {0}", exceptionType.getValue());
+        throw throwException(String.format("Unsupported exception type. Passed exception type: %s", exceptionType.getValue()));
     }
 }
