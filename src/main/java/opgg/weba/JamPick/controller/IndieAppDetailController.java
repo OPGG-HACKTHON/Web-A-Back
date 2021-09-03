40dc86e84078be0f257cbadd02068d3d804bd4f4
CHOI HEE JAE <gmlwo530@gmail.com>
2021-09-04 05:59:20 +0900

    [REFACTOR] #50 Add exception handling to IndieAppDetailController

--- src/main/java/opgg/weba/JamPick/controller/IndieAppDetailController.java
+++ src/main/java/opgg/weba/JamPick/controller/IndieAppDetailController.java
@@ -1,9 +1,11 @@
 package opgg.weba.JamPick.controller;
 
+import opgg.weba.JamPick.common.ResponseCode;
 import opgg.weba.JamPick.dto.IndieAppDetailDto;
 import opgg.weba.JamPick.dto.ResponseDto;
 import opgg.weba.JamPick.service.IndieAppDetailService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,14 +21,24 @@ public class IndieAppDetailController {
 
     @GetMapping(value = "/detail/{indieAppId}")
     public ResponseEntity<ResponseDto> getIndieAppDetail(@PathVariable("indieAppId") Long indieAppId) {
-        IndieAppDetailDto indieAppDetailDto = indieAppDetailService.getIndieAppDetail(indieAppId);
+        try {
+            IndieAppDetailDto indieAppDetailDto = indieAppDetailService.getIndieAppDetail(indieAppId);
 
-        ResponseDto responseDto = ResponseDto.builder()
-                .code(0)
-                .responseMessage("상세 조회 성공")
-                .data(indieAppDetailDto)
-                .build();
+            ResponseDto responseDto = ResponseDto.builder()
+                    .code(ResponseCode.SUCCESS.getValue())
+                    .responseMessage("상세 조회 성공")
+                    .data(indieAppDetailDto)
+                    .build();
 
-        return ResponseEntity.ok(responseDto);
+            return ResponseEntity.ok(responseDto);
+        } catch (RuntimeException e) {
+            ResponseDto responseDto = ResponseDto.builder()
+                    .code(ResponseCode.NOT_FOUND.getValue())
+                    .responseMessage(e.getMessage())
+                    .data(null)
+                    .build();
+
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body(responseDto);
+        }
     }
 }
