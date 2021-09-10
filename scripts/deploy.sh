02ec6aff2d3e6ff50b8c629dca55e4c7e403822e
CHOI HEE JAE <gmlwo530@gmail.com>
2021-09-10 21:19:37 +0900

    test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env bash
 
 REPOSITORY=/home/ubuntu/jampick
+
 cd $REPOSITORY
 
-APP_NAME=action_codedeploy
 JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep '.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
-CURRENT_PID=$(pgrep -f $APP_NAME)
+echo "> JAR Path: $JAR_PATH"
+chmod +x $JAR_PATH
 
-if [ -z $CURRENT_PID ]
+echo "> 8080 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:8080)
+
+if [ -z ${IDLE_PID} ]
 then
-  echo "> 종료할것 없음."
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
-  echo "> kill -9 $CURRENT_PID"
-  kill -15 $CURRENT_PID
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
   sleep 5
 fi
 
-echo "> $JAR_PATH 배포"
-nohup java -jar $JAR_PATH > /dev/null 2> /dev/null < /dev/null &
+echo "> $JAR_NAME 배포"
+nohup java -jar $JAR_PATH > /var/log/jampick/nohup.out 2>&1 &
--- src/main/java/opgg/weba/JamPick/config/JampickConfiguration.java
+++ src/main/java/opgg/weba/JamPick/config/JampickConfiguration.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
@@ -38,5 +39,11 @@ public class JampickConfiguration implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*");
 
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/");
     }
 }
